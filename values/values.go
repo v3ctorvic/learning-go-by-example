3c18e55e95a706ccbdf8acc6ce36808295ed53cb
v3ctorvic <vmg1977@gmail.com>
2025-01-12 23:28:16 -0700

    Added hello_world, values, variables, and constants that I worked on.

---
+++ values/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("go" + "lang") // strings, which can be added to gether with +.
+
+	fmt.Println("1+1 =", 1 + 1) // integers
+	fmt.Println("7.0/3.0 =", 7.0/3.0)	// floats
+
+	fmt.Println(true && false) // booleans with boolean operator and "&&".
+	fmt.Println(true || false) // booleans with boolean operator or "||".
+	fmt.Println(!true) // booolean with the not operator "!".
+}
---
+++ variables/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial" // creating a variable and assigning it the value of "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2 // creating b and c variable and assigning the value 1 and 2
+	fmt.Println(b, c)
+
+	var d = true // creating d variable and assigning it the value of "true"
+	fmt.Println(d)
+
+	var e int 	// creating the variable e but not assinging a value
+	fmt.Println(e)
+
+	f := "apple" // initalizing and assigning variable f to value apple.
+	fmt.Println(f)
+}
